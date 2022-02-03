b02c0304a8abde1389e6f8abf6222a9b7b6c142a
Jeston Hale <97457934+uriahisrael12@users.noreply.github.com>
2022-02-02 23:38:17 -0600

    Add files via upload
    Added comments to attempt to explain parts of the code.

---
+++ Hale_02:03:2022.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;      //Input Output device activated along with the namespace "std::..."
+
+const int rows = 5;       // Bounds of the rows on the grid (0-4)
+const int col = 5;        // Bounds of the columns on the grid. (0-4)
+void vals(int);           //
+
+int main(){
+  for (int row = 0; row < 5; row++)     // Counter/Loop
+  {
+    
+  }
+}
